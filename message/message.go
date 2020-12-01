030e169bf6c2139fd984d09d3a867335dab16ba9
NTH799 <54607484+NTH799@users.noreply.github.com>
2020-11-30 22:31:08 -0500

    Delete message.go

--- message/message.go
+++
@@ -1,7 +0,0 @@
-package message
-
-//define the message struct to contain the state value of the node as well as the current round of the algorithm
-type Message struct {
-	State float64
-	Round int
-}
